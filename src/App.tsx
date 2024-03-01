3b6f2be217b8d1d9e0f1e8d41513a411cff2e738
nitoc <daren2track@gmail.com>
2024-03-01 20:44:04 +0100

    fix:remove unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Bridge from "./components/Bridge";
 import Dashboard from "./components/Dashboard";
